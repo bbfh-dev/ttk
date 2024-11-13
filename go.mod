8b718e23c4d193da5cb94089448d5082b5da0d38
BubbleFish <daforsastudia@gmail.com>
2024-11-13 16:57:42 +0300

    feat: Make main.go set terminal dimensions

--- go.mod
+++ go.mod
@@ -2,7 +2,10 @@ module github.com/bbfh-dev/ttk
 
 go 1.23.2
 
-require github.com/charmbracelet/lipgloss v1.0.0
+require (
+	github.com/charmbracelet/lipgloss v1.0.0
+	golang.org/x/crypto v0.29.0
+)
 
 require (
 	github.com/aymanbagabas/go-osc52/v2 v2.0.1 // indirect
@@ -12,5 +15,6 @@ require (
 	github.com/mattn/go-runewidth v0.0.15 // indirect
 	github.com/muesli/termenv v0.15.2 // indirect
 	github.com/rivo/uniseg v0.4.7 // indirect
-	golang.org/x/sys v0.19.0 // indirect
+	golang.org/x/sys v0.27.0 // indirect
+	golang.org/x/term v0.26.0 // indirect
 )
--- main.go
+++ main.go
@@ -2,17 +2,25 @@ package main
 
 import (
 	"fmt"
+	"os"
 
 	"github.com/bbfh-dev/ttk/display"
 	"github.com/bbfh-dev/ttk/scene"
 	"github.com/bbfh-dev/ttk/ttk"
+	"golang.org/x/crypto/ssh/terminal"
 )
+
+const FIGLET = ` _____ _____ _  __
+|_   _|_   _| |/ /
+  | |   | | | ' <
+  |_|   |_| |_|\_\`
 
 func main() {
 	scene := scene.TtkWindow{
 		Widgets: []ttk.Widget{
+			&display.TtkArt{Content: FIGLET},
 			&display.TtkLabel{
-				Label:    "Hello World!\nSomething else",
+				Label:    "Hello World! And other species! What a wonderful long text about Fooga that is so based and fat and based, did I mention based? Anyway. Fooga. Yes, Fooga is incredible. It's so round and... fooga. Really, no other explanation.",
 				Justify:  0,
 				Width:    display.AUTO,
 				MaxWidth: display.AUTO,
@@ -20,5 +28,14 @@ func main() {
 		},
 	}
 	scene.Init()
+
+	var err error
+	ttk.Width, ttk.Height, err = terminal.GetSize(int(os.Stdin.Fd()))
+	ttk.Height -= 4 // My hardcoded prompt height™ + command
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+
 	fmt.Println(scene.Scene())
 }
