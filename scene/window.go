eaedc753fbff1078704a0cf77980f1dc3638ae80
BubbleFish <daforsastudia@gmail.com>
2024-11-13 16:57:21 +0300

    fix(scene): Make Window render all widgets instead of the 1st one

--- scene/window.go
+++ scene/window.go
@@ -1,17 +1,31 @@
 package scene
 
-import "github.com/bbfh-dev/ttk/ttk"
+import (
+	"github.com/bbfh-dev/ttk/ttk"
+	"github.com/charmbracelet/lipgloss"
+)
 
 type TtkWindow struct {
 	Widgets []ttk.Widget
+	length  int // Cached len(Widgets)
 }
 
 func (window *TtkWindow) Init() {
+	window.length = len(window.Widgets)
+
 	for _, widget := range window.Widgets {
 		widget.Init()
 	}
 }
 
 func (window *TtkWindow) Scene() string {
-	return window.Widgets[0].Render(0, 0)
+	var blocks = make([]string, window.length)
+	for i, widget := range window.Widgets {
+		blocks[i] = widget.Render(0, 0)
+	}
+
+	return lipgloss.Place(
+		ttk.Width, ttk.Height, 0.5, 0.5,
+		lipgloss.JoinVertical(0, blocks...),
+	)
 }
